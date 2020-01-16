9ace9db72b9498ae2c3e6b1cf4c2251317a05242
Gourab Saha <boyinyourbox@aol.com>
2020-01-16 13:58:06 +0530

    added logging in production

--- janatar_kolom/settings.py
+++ janatar_kolom/settings.py
@@ -62,6 +62,35 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'janatar_kolom.urls'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+     'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
